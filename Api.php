5d163b19546d13b12f57a8c0236273aa2fe02273
julien.manganne <julien.manganne@extreme-sensio.com>
2015-10-28 23:54:58 +0100

    Defined a new method to retrieve next trains list by a destination

--- Api.php
+++ Api.php
@@ -2,8 +2,6 @@
 
 namespace Juuuuuu\Sncf;
 
-require 'vendor/autoload.php';
-
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -13,43 +11,74 @@ class Api
     private $codeUIC;
     private $username;
     private $password;
+    private $destinations = [];
 
-    public function __construct($codeUIC, $username, $password)
+    public function __construct($codeUIC, $username, $password, array $destinations = [])
     {
         $this->codeUIC = $codeUIC;
         $this->username = $username;
         $this->password = $password;
-
+        $this->destinations = $destinations;
         $this->client = new Client();
-        $response = $this->client->request('GET', 'http://api.transilien.com/gare/'.$this->codeUIC.'/depart/', [
-            'auth' => [$this->username, $this->password],
-            'headers' => [
-                'Accept' => 'application/vnd.sncf.transilien.od.depart+xml;vers=1'
-            ],
-        ]);
+    }
 
-        if ($response->getStatusCode() == 200) {
-            $body = $response->getBody();
-            $crawler = new Crawler($body->__toString());
+    public function getNextTrains()
+    {
+        try {
+            $response = $this->client->request('GET', 'http://api.transilien.com/gare/'.$this->codeUIC.'/depart/', [
+                'auth' => [$this->username, $this->password],
+                'headers' => [
+                    'Accept' => 'application/vnd.sncf.transilien.od.depart+xml;vers=1'
+                ],
+            ]);
 
-            foreach ($crawler as $passages) {
-                foreach ($passages->childNodes as $passage) {
-                    if ($passage->nodeName == 'train') {
-                        $train = array();
+            if ($response->getStatusCode() == 200) {
+                $trains = [];
+                $body = $response->getBody();
 
-                        foreach ($passage->childNodes as $data) {
-                            var_export($data);
+                $crawler = new Crawler($body->__toString());
+
+                foreach ($crawler as $passages) {
+                    foreach ($passages->childNodes as $key => $passage) {
+                        if ($passage->nodeName == 'train') {
+                            foreach ($passage->childNodes as $data) {
+                                if ($data->nodeName == 'date') {
+                                    $trains[$key]['date'] = $data->nodeValue;
+                                }
+                                if ($data->nodeName == 'num') {
+                                    $trains[$key]['num'] = $data->nodeValue;
+                                }
+                                if ($data->nodeName == 'miss') {
+                                    $trains[$key]['miss'] = $data->nodeValue;
+                                }
+                                if ($data->nodeName == 'term') {
+                                    $trains[$key]['term'] = $data->nodeValue;
+                                }
+                                if ($data->nodeName == 'etat') {
+                                    $trains[$key]['etat'] = $data->nodeValue;
+                                }
+                            }
                         }
                     }
                 }
+
+                return $this->filterByDestination($trains);
+            }
+        } catch (Exception $e) {
+            return json_encode(['status' => 520, 'message' => $e->getMessage()]);
+        }
+
+        return json_encode(['status' => 400, 'message' => 'Bad request']);
+    }
+
+    private function filterByDestination($trains)
+    {
+        foreach ($trains as $key => $train) {
+            if (!in_array($train['term'], $this->destinations)) {
+                unset($trains[$key]);
             }
         }
-    }
 
-    public function getNextTrains($station)
-    {
+        return $trains;
     }
-
 }
-
-$api = new Api('87386003', '', '');
--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 Sncf SDK
 ========
 
-This repository contains a small library to consume Sncf web service.
+This repository contains a small library to consume Sncf web service in order to get new trains at a given station.
 
-Usage
 
+First and last arguments are UIC codes and can be found [here][UIC].
+It's a unique ID defining a station.
+
+Usage:
 ```php
-$api = new Juuuuuu\Sncf\Api();
-... 
+$api = new Juuuuuu\Sncf\Api('87386003', 'username', 'password', ['87384008']);
+$trains = $api->getNextTrains();
 ```
+
+[UIC]: https://ressources.data.sncf.com/explore/dataset/referentiel-gares-voyageurs
